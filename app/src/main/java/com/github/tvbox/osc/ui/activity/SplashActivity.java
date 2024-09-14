2bdca77e18928d87cba3232fca370a65ef5f7bf9
linf <linf@tradingvisual.com>
2024-09-14 23:15:33 +0800

    add:增加启动屏

---
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.github.tvbox.osc.ui.activity;
+import com.github.tvbox.osc.R;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_splash);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
